d19b13097836c5d348d87b1b1c03f746c6185822
hungcndev <Hung_Cao@mckinsey.com>
2024-04-07 21:30:37 +0700

    updated get_data

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 ## Instruction to run
 
-- Import `main.ipynb` to Google Colab
-- Upload this repo to Google Colab content folder
-- Execute cells in `main.ipynb` to run the scripts for:
+- Import the notebook `main.ipynb` to Google Colab
+- Change Colab Runtime Type to GPU
+- Upload this repo to the current Google Colab content directory
+- Execute cells in the notebook to run the scripts for:
+  - Prepare model checkpoints and dataset
   - Train/test the GMM model
   - Train/test the TOM model
   - Inference the end-to-end of the CP-VTON for test data using GMM and TOM models.
--- get_data.py
+++ get_data.py
@@ -3,6 +3,7 @@ import tarfile
 import shutil
 import gdown
 
+# Download and prepare dataset for training and testing
 
 url = "https://drive.google.com/uc?export=download&confirm=CONFIRM&id=1MxCUvKxejnwWnoZ-KoCyMCXo3TLhRuTo"
 output = "data/viton_resize.tar.gz"
@@ -16,3 +17,16 @@ shutil.move('data/viton_resize/train/', 'data/train/')
 os.rmdir('data/viton_resize/')
 os.remove('data/viton_resize.tar.gz')
 
+
+# Download and prepare checkpoints for GMM and TOM models
+
+gmm_url = "https://drive.google.com/file/d/1lGPLTDEuRgYvdJIgS4L0qeAp7LQn6hIr/view"
+gmm_file = "gmm_final.pth"
+gdown.download(gmm_url, gmm_file, quiet=False)
+shutil.move(gmm_file, 'checkpoints/GMM/')
+
+
+tom_url = "https://drive.google.com/file/d/10wnB1coU73u3rbRgevHRyadO7b1tCron/view"
+tom_file = "tom_final.pth"
+gdown.download(tom_url, tom_file, quiet=False)
+shutil.move(tom_file, 'checkpoints/TOM/')
